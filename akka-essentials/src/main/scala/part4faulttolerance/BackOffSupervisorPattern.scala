9efa6259442f0fb93fe35096a8d1c5a0c2c8372e
tim.shen <tim.shen@farfetch.com>
2022-05-14 11:14:22 +0800

    upload BackOffSupervisorPattern.scala

---
+++ akka-essentials/src/main/scala/part4faulttolerance/BackOffSupervisorPattern.scala
@@ -0,0 +1,88 @@
+package part4faulttolerance
+
+import akka.actor.SupervisorStrategy._
+import akka.actor._
+import akka.pattern._
+
+import scala.concurrent.duration._
+import scala.io.Source
+import java.io.File
+import scala.language.postfixOps
+
+object BackOffSupervisorPattern extends App {
+
+  case object ReadFile
+
+  class FileBasedPersistentActor extends Actor with ActorLogging {
+
+    var dataSource: Source = null
+
+    override def preStart(): Unit = {
+      log.info("Persistent actor starting")
+    }
+
+    override def postStop(): Unit = {
+      log.warning("Persistent actor has stopped")
+    }
+
+    override def preRestart(reason: Throwable, message: Option[Any]): Unit = {
+      log.warning("Persistent actor restarting")
+    }
+
+    override def receive: Receive = {
+      case ReadFile =>
+        if (dataSource == null) {
+          /**
+           * Notice the file path here!!
+           */
+          dataSource = Source.fromFile(new File("akka-essentials/src/main/resources/testfiles/important_data.txt"))
+        }
+        log.info(s"I've just read some important data: ${dataSource.getLines().toList}")
+
+    }
+  }
+
+  val system = ActorSystem("BackoffSupervisorDemo")
+  val simpleActor = system.actorOf(Props[FileBasedPersistentActor], "simpleActor")
+//  simpleActor ! ReadFile
+
+  val simpleSupervisorProbes: Props = BackoffSupervisor.props(
+    BackoffOpts.onFailure(
+      Props[FileBasedPersistentActor],
+      "simpleBackoffActor",
+      3 seconds, // then 6s, 12s, 24s,...,
+      30 seconds,
+      0.2
+    )
+  )
+
+  val simpleBackoffSupervisor = system.actorOf(simpleSupervisorProbes, "simpleSupervisor") // 创建时，子 child 也一起创建了
+//  simpleBackoffSupervisor ! ReadFile // simpleSupervisor will forward to its child
+
+  /**
+   * simpleSupervisor
+   *  - first creates a child called simpleBackoffActor (props of type FileBasedPersistentActor)
+   *  - the simpleSupervisor can receive any message and forward them to its child
+   *  - supervision strategy is the default one (namely, restarting on everything)
+   *    - when it fails:
+   *      - first attempt after 3 seconds
+   *      - next attempt is 2x the previous attempt, i.e. 3s then 6s, 12s, 24s,...,
+   *      - 0.2 is the distraction factor, to prevent huge amount of child restart at the same time
+   */
+
+
+  val stopSupervisorProps = BackoffSupervisor.props(
+    BackoffOpts.onStop (
+      Props[FileBasedPersistentActor],
+      "stopBackoffActor",
+      3 seconds,
+      30 seconds,
+      0.2
+    ).withSupervisorStrategy(OneForOneStrategy() {
+      case _ => Stop
+    })
+  )
+
+  val stopSupervisor = system.actorOf(stopSupervisorProps, "stopSupervisor")
+  stopSupervisor ! ReadFile
+}
